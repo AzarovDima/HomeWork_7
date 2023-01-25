8779cc372ba89664e3ba78e6349f6842aa61ae5e
AzarovDima <ada89885308140@gmail.com>
2023-01-25 23:05:03 +0300

    Решили 1 задачу

--- question_1_double/Program.cs
+++ question_1_double/Program.cs
@@ -6,18 +6,57 @@ m = 3, n = 4.
 8 7,8 -7,1 9
  */
 
+int rows = GetNumbetFromUserM("Какое количество строк в массиве задашь? ", "Ошибка ввода! ");
+int columns = GetNumbetFromUserN("А количество столбцов? ", "Ошибка ввода! ");
+Console.WriteLine($"Генирирую массив заполненный случайными вещественными числами... ");
+double[,] array = GetArray(rows, columns, -10, 10);
 
-int numM = GetNumbetFromUser("Введите целоечисло m: ", "Ошибка ввода! ");
-
-Console.WriteLine(numM);
-int GetNumbetFromUser(string message, string errorMessage)
+int GetNumbetFromUserN(string message, string errorMessage)
 {
     while (true)
     {
         Console.Write(message);
-        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumberN);
         if (isCorrect)
-            return userNumber;
+            return userNumberN;
         Console.WriteLine(errorMessage);
     }
 }
+int GetNumbetFromUserM(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumberM);
+        if (isCorrect)
+            return userNumberM;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+    var rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(minValue + rnd.NextDouble() * (maxValue - minValue), 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(array);
