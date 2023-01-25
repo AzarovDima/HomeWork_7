c60c449a8fc6c0d5f6af8b012773095b90f7eea4
AzarovDima <ada89885308140@gmail.com>
2023-01-25 22:03:23 +0300

    Решаем ДЗ вспоминаем методы

---
+++ question_1_double/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+!!!ПРИМЕР!!!
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+ */
+
+
+int numM = GetNumbetFromUser("Введите целоечисло m: ", "Ошибка ввода! ");
+
+Console.WriteLine(numM);
+int GetNumbetFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
